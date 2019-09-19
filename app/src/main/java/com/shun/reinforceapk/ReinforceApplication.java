2d258df051af8d5bfe85f32218b9a75d93bd5334
xushun <xushun@shgbit.com>
2019-09-19 09:32:12 +0800

    first commit

---
+++ app/src/main/java/com/shun/reinforceapk/ReinforceApplication.java
@@ -0,0 +1,16 @@
+package com.shun.reinforceapk;
+
+import com.shun.protectapp.ShellApplication;
+
+/**
+ * Created by xushun on  2019/9/18 16:53.
+ * Email：shunplus@163.com
+ * Des：
+ */
+public class ReinforceApplication extends ShellApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //处理业务
+    }
+}
